36c1a9ce100449e1c23c5dd4e85da00ab6975940
unknown <ibrahimcadabra1@gmail.com>
2025-12-17 12:23:59 +0300

    Fix syntax error in REST_FRAMEWORK settings

--- eduka_backend/settings.py
+++ eduka_backend/settings.py
@@ -180,8 +180,6 @@ AUTHENTICATION_BACKENDS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
